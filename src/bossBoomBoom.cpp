387a7b3ad95aafc9c51f9f9744b6dd31657d3fd6
RedStoneMatt <13626901+RedStoneMatt@users.noreply.github.com>
2019-04-18 21:20:35 +0200

    Added Main Boom Boom Features

--- src/bossBoomBoom.cpp
+++ src/bossBoomBoom.cpp
@@ -19,10 +19,11 @@ public:
 
 	mHeapAllocator_c allocator;
 	nw4r::g3d::ResFile resFile;
+	m3d::anmChr_c animationChr;
+	m3d::anmClr_c animationClr;
 
 	m3d::mdl_c bodyModel;
 
-	m3d::anmChr_c chrAnimation;
 
 	char isDown;
 	float XSpeed;
@@ -35,12 +36,14 @@ public:
 	int isTurningCountdown;
 	char charging;
 	int flashing;
+	int directiontomove;
 
 	float dying;
 
 	static daBunbun *build();
 
 	void bindAnimChr_and_setUpdateRate(const char* name, int unk, float unk2, float rate);
+	void bindAnimClr_and_setUpdateRate(const char* name, int unk, float unk2, float rate);
 	void updateModelMatrices();
 	bool calculateTileCollisions();
 
@@ -64,17 +67,12 @@ public:
 	void addScoreWhenHit(void *other);
 
 	USING_STATES(daBunbun);
-	DECLARE_STATE(Walk);
-	DECLARE_STATE(Turn);
-	DECLARE_STATE(KnockBack);
-
-	DECLARE_STATE(ChargePrep);
+	DECLARE_STATE(Intro);
 	DECLARE_STATE(Charge);
-	DECLARE_STATE(ChargeRecover);
+	DECLARE_STATE(Turn);
 	DECLARE_STATE(Damage);
-
-	DECLARE_STATE(Grow);
 	DECLARE_STATE(Outro);
+
 
 };
 
@@ -91,114 +89,44 @@ daBunbun *daBunbun::build() {
 	extern "C" int SmoothRotation(short* rot, u16 amt, int unk2);
 
 
-	CREATE_STATE(daBunbun, Walk);
-	CREATE_STATE(daBunbun, Turn);
-	CREATE_STATE(daBunbun, KnockBack);
-
-	CREATE_STATE(daBunbun, ChargePrep);
+	CREATE_STATE(daBunbun, Intro);
 	CREATE_STATE(daBunbun, Charge);
-	CREATE_STATE(daBunbun, ChargeRecover);
+	CREATE_STATE(daBunbun, Turn);
 	CREATE_STATE(daBunbun, Damage);
-
-	CREATE_STATE(daBunbun, Grow);
 	CREATE_STATE(daBunbun, Outro);
-
-	// 	begoman_attack2"	// wobble back and forth tilted forwards
-	// 	begoman_attack3"	// Leaned forward, antennae extended
-	// 	begoman_damage"		// Bounces back slightly
-	// 	begoman_damage2"	// Stops spinning and wobbles to the ground like a top
-	// 	begoman_stand"		// Stands still, waiting
-	// 	begoman_wait"		// Dizzily Wobbles
-	// 	begoman_wait2"		// spins around just slightly
-	// 	begoman_attack"		// Rocks backwards, and then attacks to an upright position, pulsing out his antennae
 
 
 ////////////////////////
 // Collision Functions
 ////////////////////////
-	// void topCollisionCallback(ActivePhysics *apThis, ActivePhysics *apOther);
 
-	// void topCollisionCallback(ActivePhysics *apThis, ActivePhysics *apOther) {
-	// 	OSReport("Collided with %d", apOther->owner->name);
-	// 	if (apOther->owner->name != 041) {
-	// 		dEn_c::collisionCallback(apThis, apOther);
-	// 	}
-	// 	else {
-	// 		daBunbun *actor = (daBunbun*)apThis->owner;
-	// 		actor->damage += 1;
-	// 		actor->doStateChange(&daBunbun::StateID_Damage);
-
-	// 		if (actor->damage > 2) { actor->doStateChange(&daBunbun::StateID_Outro); }
-	// 	}
-	// }
 	void daBunbun::addScoreWhenHit(void *other) { };
 
 	void daBunbun::spriteCollision(ActivePhysics *apThis, ActivePhysics *apOther) {
-		if (apOther->owner->name == 390) { //time to get hurt
-			OSReport("YO SUP I'M A TOPMAN AND I'M COLLIDING WITH A FUCKING WALL [%d]\n", damage);
-			if (this->isInvulnerable) {
-				OSReport("I'm invulnerable so I'm ignoring it\n");
-				return;
-			}
-			this->damage += 1;
-			OSReport("I'm increasing my damage to %d\n", damage);
-			doStateChange(&StateID_Damage);
+		// if (apOther->owner->name == 390) { //time to get hurt
+			// OSReport("YO SUP I'M A TOPMAN AND I'M COLLIDING WITH A FUCKING WALL [%d]\n", damage);
+			// if (this->isInvulnerable) {
+				// OSReport("I'm invulnerable so I'm ignoring it\n");
+				// return;
+			// }
+			// this->damage += 1;
+			// OSReport("I'm increasing my damage to %d\n", damage);
+			// doStateChange(&StateID_Damage);
 
-			if (this->damage == 3) { doStateChange(&StateID_Outro); }
-		}
-		else { dEn_c::spriteCollision(apThis, apOther); }
+			// if (this->damage == 3) { doStateChange(&StateID_Outro); }
+		// }
+		// else { dEn_c::spriteCollision(apThis, apOther); }
 	}
 
 	void daBunbun::playerCollision(ActivePhysics *apThis, ActivePhysics *apOther) {
-
-
 		char hitType;
 		hitType = usedForDeterminingStatePress_or_playerCollision(this, apThis, apOther, 0);
-
+		
 		if(hitType > 0) {
+			doStateChange(&StateID_Damage);
+		}
+		else {
 			this->_vf220(apOther->owner);
-			this->counter_504[apOther->owner->which_player] = 0;
-		} else {
-
-			if (this->charging == 1) {
-
-				char MarioPos;
-
-				if (this->pos.x < apOther->owner->pos.x) { MarioPos = 0; } // Mario is to the right
-				else 									 { MarioPos = 1; } // Mario is to the left
-
-
-				if (this->direction != MarioPos) {	// Mario is stnading behind the boss
-					EN_LandbarrelPlayerCollision(this, apThis, apOther);
-					if (MarioPos == 1)  { this->direction = 1; }
-					else 				{ this->direction = 0; }
-
-					doStateChange(&StateID_KnockBack);
-				}
-
-				else { // Mario is standing in front of the boss
-					this->_vf220(apOther->owner);
-					EN_LandbarrelPlayerCollision(this, apThis, apOther);
-				}
-			}
-
-			else if (this->isInvulnerable == 0) {
-
-				if (this->pos.x > apOther->owner->pos.x) {
-					this->direction = 1;
-				}
-				else {
-					this->direction = 0;
-				}
-
-				this->counter_504[apOther->owner->which_player] = 0;
-				EN_LandbarrelPlayerCollision(this, apThis, apOther);
-				doStateChange(&StateID_KnockBack);
-			}
-			else {
-				this->counter_504[apOther->owner->which_player] = 0;
-				EN_LandbarrelPlayerCollision(this, apThis, apOther);
-			}
 		}
 
 		deathInfo.isDead = 0;
@@ -210,28 +138,23 @@ daBunbun *daBunbun::build() {
 	}
 
 	bool daBunbun::collisionCatD_Drill(ActivePhysics *apThis, ActivePhysics *apOther) {
-		DamagePlayer(this, apThis, apOther);
+		this->playerCollision(apThis, apOther);
 		return true;
 	}
 	bool daBunbun::collisionCat7_GroundPound(ActivePhysics *apThis, ActivePhysics *apOther) {
-		DamagePlayer(this, apThis, apOther);
+		this->playerCollision(apThis, apOther);
 		return true;
 	}
 	bool daBunbun::collisionCat7_GroundPoundYoshi(ActivePhysics *apThis, ActivePhysics *apOther) {
-		DamagePlayer(this, apThis, apOther);
+		this->playerCollision(apThis, apOther);
 		return true;
 	}
 
 	bool daBunbun::collisionCat9_RollingObject(ActivePhysics *apThis, ActivePhysics *apOther) {
-		if (this->isInvulnerable == 0) {
-			doStateChange(&StateID_KnockBack);
-		}
 		return true;
 	}
 	bool daBunbun::collisionCatA_PenguinMario(ActivePhysics *apThis, ActivePhysics *apOther){
-		if (this->isInvulnerable == 0) {
-			doStateChange(&StateID_KnockBack);
-		}
+		DamagePlayer(this, apThis, apOther);
 		return true;
 	}
 
@@ -317,9 +240,15 @@ bool daBunbun::calculateTileCollisions() {
 
 void daBunbun::bindAnimChr_and_setUpdateRate(const char* name, int unk, float unk2, float rate) {
 	nw4r::g3d::ResAnmChr anmChr = this->resFile.GetResAnmChr(name);
-	this->chrAnimation.bind(&this->bodyModel, anmChr, unk);
-	this->bodyModel.bindAnim(&this->chrAnimation, unk2);
-	this->chrAnimation.setUpdateRate(rate);
+	this->animationChr.bind(&this->bodyModel, anmChr, unk);
+	this->bodyModel.bindAnim(&this->animationChr, unk2);
+	this->animationChr.setUpdateRate(rate);
+}
+
+void daBunbun::bindAnimClr_and_setUpdateRate(const char* name, int unk, float unk2, float rate) {
+	nw4r::g3d::ResAnmClr anmRes = this->resFile.GetResAnmClr(name);
+	this->animationClr.bind(&this->bodyModel, anmRes, 0, 0);
+	this->bodyModel.bindAnim(&this->animationClr, 0.0);
 }
 
 int daBunbun::onCreate() {
@@ -327,22 +256,26 @@ int daBunbun::onCreate() {
 	// Model creation
 	allocator.link(-1, GameHeaps[0], 0, 0x20);
 
-	this->resFile.data = getResource("topman", "g3d/begoman_spike.brres");
-	nw4r::g3d::ResMdl mdl = this->resFile.GetResMdl("begoman");
+	this->resFile.data = getResource("bunbun", "g3d/bunbun.brres");
+	nw4r::g3d::ResMdl mdl = this->resFile.GetResMdl("bunbun");
 	bodyModel.setup(mdl, &allocator, 0x224, 1, 0);
 	SetupTextures_Map(&bodyModel, 0);
 
 
 	// Animations start here
-	nw4r::g3d::ResAnmChr anmChr = this->resFile.GetResAnmChr("begoman_wait");
-	this->chrAnimation.setup(mdl, anmChr, &this->allocator, 0);
+	nw4r::g3d::ResAnmChr anmChr = this->resFile.GetResAnmChr("wait");
+	this->animationChr.setup(mdl, anmChr, &this->allocator, 0);
+
+	nw4r::g3d::ResAnmClr anmRess = this->resFile.GetResAnmClr("normal");
+	this->animationClr.setup(mdl, anmRess, &this->allocator, 0, 1);
+	this->animationClr.bind(&this->bodyModel, anmRess, 0, 0);
+	this->bodyModel.bindAnim(&this->animationClr, 0.0);
 
 	allocator.unlink();
 
 	// Stuff I do understand
-	this->scale = (Vec){0.2, 0.2, 0.2};
+	this->scale = (Vec){1, 1, 1};
 
-	// this->pos.y = this->pos.y + 30.0; // X is vertical axis
 	this->rot.x = 0; // X is vertical axis
 	this->rot.y = 0xD800; // Y is horizontal axis
 	this->rot.z = 0; // Z is ... an axis >.>
@@ -364,8 +297,8 @@ int daBunbun::onCreate() {
 	HitMeBaby.xDistToCenter = 0.0;
 	HitMeBaby.yDistToCenter = 24.0;
 
-	HitMeBaby.xDistToEdge = 28.0;
-	HitMeBaby.yDistToEdge = 24.0;
+	HitMeBaby.xDistToEdge = 15.0;
+	HitMeBaby.yDistToEdge = 15.0;
 
 	HitMeBaby.category1 = 0x3;
 	HitMeBaby.category2 = 0x0;
@@ -403,8 +336,9 @@ int daBunbun::onCreate() {
 
 
 	// State Changers
-	bindAnimChr_and_setUpdateRate("begoman_wait2", 1, 0.0, 1.0);
-	doStateChange(&StateID_Grow);
+	bindAnimChr_and_setUpdateRate("wait", 1, 0.0, 1.0);
+	bindAnimClr_and_setUpdateRate("normal", 1, 0.0, 1.0);
+	doStateChange(&StateID_Intro);
 
 	this->onExecute();
 	return true;
@@ -450,80 +384,38 @@ void daBunbun::updateModelMatrices() {
 
 
 ///////////////
-// Grow State
+// Intro State
 ///////////////
-	void daBunbun::beginState_Grow() {
-		bindAnimChr_and_setUpdateRate("begoman_wait2", 1, 0.0, 0.75);
+	void daBunbun::beginState_Intro() {
+		bindAnimChr_and_setUpdateRate("wait", 1, 0.0, 0.75);
 
 		this->timer = 0;
 
 		SetupKameck(this, Kameck);
 	}
 
-	void daBunbun::executeState_Grow() {
+	void daBunbun::executeState_Intro() {
 
-		if(this->chrAnimation.isAnimationDone()) {
-			this->chrAnimation.setCurrentFrame(0.0);
+		if(this->animationChr.isAnimationDone()) {
+			this->animationChr.setCurrentFrame(0.0);
 		}
 
 		this->timer += 1;
 
-		bool ret;
-		ret = GrowBoss(this, Kameck, 0.2, 0.4, 0, this->timer);
-
-		if (ret) {
+		if(this->timer == 1) {
+			bindAnimClr_and_setUpdateRate("star_color", 1, 0.0, 0.75);
 			PlaySound(this, SE_EMY_MECHAKOOPA_BOUND);
-			doStateChange(&StateID_Walk);
+		}
+		if(this->timer == 150) {
+			doStateChange(&StateID_Charge);
 		}
 	}
-	void daBunbun::endState_Grow() {
-		this->chrAnimation.setUpdateRate(1.0);
+	void daBunbun::endState_Intro() {
+		this->animationChr.setUpdateRate(1.0);
 		CleanupKameck(this, Kameck);
 	}
 
 
-///////////////
-// Walk State
-///////////////
-	void daBunbun::beginState_Walk() {
-		this->max_speed.x = (this->direction) ? -this->XSpeed : this->XSpeed;
-		this->speed.x = (direction) ? -1.2f : 1.2f;
-
-		this->max_speed.y = (this->isInSpace) ? -2.0 : -4.0;
-		this->speed.y = 	(this->isInSpace) ? -2.0 : -4.0;
-		this->y_speed_inc = (this->isInSpace) ? -0.09375 : -0.1875;
-
-		this->isTurningCountdown = 0;
-	}
-	void daBunbun::executeState_Walk() {
-
-		if (this->isInvulnerableCountdown > 0) {
-			this->isInvulnerableCountdown--;
-		}
-		else {
-			this->isInvulnerable = 0;
-			this->flashing = 0;
-		}
-
-		PlaySound(this, SE_BOSS_JR_CROWN_JR_RIDE); // 5
-
-		bool ret = calculateTileCollisions();
-		if (ret) {
-			doStateChange(&StateID_Turn);
-		}
-
-		u8 dir = dSprite_c__getXDirectionOfFurthestPlayerRelativeToVEC3(this, this->pos);
-		if (dir != this->direction) {
-			this->isTurningCountdown++;
-		}
-
-		if (this->isTurningCountdown > 60) { doStateChange(&StateID_Turn); }
-
-		if(this->chrAnimation.isAnimationDone()) {
-			this->chrAnimation.setCurrentFrame(0.0);
-		}
-	}
-	void daBunbun::endState_Walk() { this->timer += 1; }
 
 
 ///////////////
@@ -535,140 +427,53 @@ void daBunbun::updateModelMatrices() {
 	}
 	void daBunbun::executeState_Turn() {
 
-		if(this->chrAnimation.isAnimationDone()) {
-			this->chrAnimation.setCurrentFrame(0.0);
+		if(this->animationChr.isAnimationDone()) {
+			this->animationChr.setCurrentFrame(0.0);
 		}
 
 		u16 amt = (this->direction == 0) ? 0x2800 : 0xD800;
 		int done = SmoothRotation(&this->rot.y, amt, 0x800);
 
 		if(done) {
-			if (this->damage > 0) 	{ doStateChange(&StateID_ChargePrep); }
-			else 					{ doStateChange(&StateID_Walk); }
+			doStateChange(&StateID_Charge);
 		}
 	}
 	void daBunbun::endState_Turn() { this->rot.y = (this->direction) ? 0xD800 : 0x2800; }
 
 
-///////////////
-// Knockback State
-///////////////
-	void daBunbun::beginState_KnockBack() {
-		bindAnimChr_and_setUpdateRate("begoman_damage", 1, 0.0, 0.65);
-
-		this->max_speed.x = (this->direction) ? 6.5f : -6.5f;
-		this->speed.x = (this->direction) ? 6.5f : -6.5f;
-	}
-	void daBunbun::executeState_KnockBack() {
-
-		bool ret = calculateTileCollisions();
-		if (ret) {
-			this->max_speed.x = -this->max_speed.x;
-			this->speed.x = -this->speed.x;
-		}
-		this->speed.x = this->speed.x / 1.08;
-
-		bodyModel._vf1C();
-		if(this->chrAnimation.isAnimationDone()) {
-			if (this->damage > 0) 	{ doStateChange(&StateID_ChargePrep); }
-			else 					{ doStateChange(&StateID_Walk); }
-		}
-
-	}
-	void daBunbun::endState_KnockBack() {
-		if (this->rot.y == 0x2800) {
-			// CreateEffect(&this->pos, &(S16Vec){0,0,0}, &(Vec){1.0, 1.0, 1.0}, 175);
-			this->direction = 0;
-		}
-		else {
-			// CreateEffect(&this->pos, &(S16Vec){0,0,0}, &(Vec){1.0, 1.0, 1.0}, 192);
-			this->direction = 1;
-		}
-		// this->direction ^= 1;
-		bindAnimChr_and_setUpdateRate("begoman_wait2", 1, 0.0, 1.0);
-	}
-
-
-///////////////
-// ChargePrep State
-///////////////
-	void daBunbun::beginState_ChargePrep() {
-		bindAnimChr_and_setUpdateRate("begoman_attack", 1, 0.0, 0.9);
-	}
-	void daBunbun::executeState_ChargePrep() {
-		if(this->chrAnimation.isAnimationDone()) {
-			doStateChange(&StateID_Charge);
-		}
-	}
-	void daBunbun::endState_ChargePrep() { }
 
 
 ///////////////
 // Charge State
 ///////////////
 	void daBunbun::beginState_Charge() {
-		bindAnimChr_and_setUpdateRate("begoman_attack3", 1, 0.0, 1.0);
+		bindAnimChr_and_setUpdateRate("run", 1, 0.0, 1.0);
 		this->timer = 0;
 		this->isTurningCountdown = 0;
-
-		this->max_speed.x = (this->direction) ? -this->XSpeed : this->XSpeed;
-		this->speed.x = (direction) ? -2.6f : 2.6f;
-
-		this->max_speed.y = (this->isInSpace) ? -2.0 : -4.0;
-		this->speed.y = 	(this->isInSpace) ? -2.0 : -4.0;
-		this->y_speed_inc = (this->isInSpace) ? -0.09375 : -0.1875;
 
 		this->charging = 1;
 	}
 	void daBunbun::executeState_Charge() {
-
-		if (this->isInvulnerableCountdown > 0) {
-			this->isInvulnerableCountdown--;
-		}
-		else {
-			this->isInvulnerable = 0;
-			this->flashing = 0;
-		}
-
-		if(this->chrAnimation.isAnimationDone()) {
-			this->chrAnimation.setCurrentFrame(0.0);
+		if(this->animationChr.isAnimationDone()) {
+			this->animationChr.setCurrentFrame(0.0);
 		}
 
 		bool ret = calculateTileCollisions();
 		if (ret) {
-			doStateChange(&StateID_ChargeRecover);
+			doStateChange(&StateID_Turn);
 		}
-
-		u8 dir = dSprite_c__getXDirectionOfFurthestPlayerRelativeToVEC3(this, this->pos);
-		if (dir != this->direction) {
-			this->isTurningCountdown++;
-			this->speed.x = this->speed.x / 1.04;
+		if(this->rot.y == 0xD800) {
+			this->directiontomove = -1;
 		}
-
-		if (this->isTurningCountdown > 90) { doStateChange(&StateID_Turn); }
+		if(this->rot.y == 0x2800) {
+			this->directiontomove = 1;
+		}
+		this->pos.x += 4 * this->directiontomove;
 	}
 	void daBunbun::endState_Charge() {
 		this->charging = 0;
-
-		this->counter_504[0] = 0;
-		this->counter_504[1] = 0;
-		this->counter_504[2] = 0;
-		this->counter_504[3] = 0;
 	}
 
-
-///////////////
-// ChargeRecover State
-///////////////
-	void daBunbun::beginState_ChargeRecover() {
-		bindAnimChr_and_setUpdateRate("begoman_stand", 1, 0.0, 0.5);
-	}
-	void daBunbun::executeState_ChargeRecover() {
-		if(this->chrAnimation.isAnimationDone()) {
-			doStateChange(&StateID_Turn);
-		}
-	}
-	void daBunbun::endState_ChargeRecover() { }
 
 
 ///////////////
@@ -676,47 +481,34 @@ void daBunbun::updateModelMatrices() {
 ///////////////
 	void daBunbun::beginState_Damage() {
 		this->isInvulnerable = 1;
-		bindAnimChr_and_setUpdateRate("begoman_wait", 1, 0.0, 0.75);
+		bindAnimChr_and_setUpdateRate("damage", 1, 0.0, 0.75);
+		bindAnimClr_and_setUpdateRate("damage", 1, 0.0, 0.75);
 		this->timer = 0;
-
-		PlaySound(this, SE_PLY_TOUCH_BIRI);
-		PlaySound(this, SE_BOSS_JR_ELEC_SPARK);
-		PlaySound(this, SE_EMY_BIRIKYU_SPARK);
-
-		Vec front = {this->pos.x-4.0f, this->pos.y+8.0f, 5500.0f};
-		Vec back = {this->pos.x-4.0f, this->pos.y+8.0f, -5500.0f};
-
-		Vec sparkScale = {2.5f, 2.5f, 1.5f};
-		SpawnEffect("Wm_ob_cmnspark", 0, &front, 0, &sparkScale);
-		Vec lineScale = {1.5f, 1.5f, 1.5f};
-		SpawnEffect("Wm_mr_wirehit_line", 0, &front, 0, &lineScale);
-		Vec kickScale = {1.5f, 1.5f, 1.5f};
-		SpawnEffect("Wm_mr_kick_grain", 0, &front, 0, &kickScale);
-		Vec glowScale = {2.5f, 2.5f, 2.5f};
-		SpawnEffect("Wm_mr_wirehit_glow", 0, &back, 0, &glowScale);
+		this->damage++;
 	}
 	void daBunbun::executeState_Damage() {
-
-		if(this->chrAnimation.isAnimationDone()) {
-			this->timer += 1;
-			if (this->timer == 1) {
-
-				u8 dir = dSprite_c__getXDirectionOfFurthestPlayerRelativeToVEC3(this, this->pos);
-				if (dir != this->direction) {
-					doStateChange(&StateID_Turn);
+		if(this->damage < 3) {
+			if(this->animationChr.isAnimationDone()) {
+				this->timer += 1;
+				if (this->timer == 1) {
+					u8 dir = dSprite_c__getXDirectionOfFurthestPlayerRelativeToVEC3(this, this->pos);
+					if (dir != this->direction) {
+						doStateChange(&StateID_Turn);
+					}
+					else {
+						doStateChange(&StateID_Charge);
+					}
 				}
-				else {
-					if (this->damage > 1) 	{ doStateChange(&StateID_ChargePrep); }
-					else 					{ doStateChange(&StateID_Walk); }
-				}
+				this->animationChr.setCurrentFrame(0.0);
 			}
-			else {
-			}
-			this->chrAnimation.setCurrentFrame(0.0);
+		}
+		else {
+			doStateChange(&StateID_Outro);
 		}
 	}
 	void daBunbun::endState_Damage() {
-		bindAnimChr_and_setUpdateRate("begoman_wait2", 1, 0.0, 1.0);
+		bindAnimChr_and_setUpdateRate("run", 1, 0.0, 1.0);
+		bindAnimClr_and_setUpdateRate("normal", 1, 0.0, 0.75);
 		this->isInvulnerableCountdown = 90;
 	}
 
@@ -726,6 +518,7 @@ void daBunbun::updateModelMatrices() {
 ///////////////
 	void daBunbun::beginState_Outro() {
 		OutroSetup(this);
+		bindAnimChr_and_setUpdateRate("dead", 1, 0.0, 1.0);
 	}
 	void daBunbun::executeState_Outro() {
 
